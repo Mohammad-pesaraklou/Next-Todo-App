aba44a3037cd2b19c88c8273af698ddd745eb4c7
Mohammad Pesaraklou <mhmdpsr6@gmail.com>
2023-05-17 02:59:46 +0330

    final commit

--- components/modules/TodoPage.js
+++ components/modules/TodoPage.js
@@ -1,6 +1,5 @@
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import axios from "axios";
 // style
 import styles from "../../styles/TodoCard.module.scss";
